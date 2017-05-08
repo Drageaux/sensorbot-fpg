0dc58126263515242da2fe0b9f0ffdc881865ecb
David Thong Nguyen <thnguyen2808@gmail.com>
2017-05-08 16:24:20 -0400

    Connecting and setup for 1 device works

--- app.js
+++ app.js
@@ -1,19 +1,54 @@
 var express = require('express'),
     path = require('path'),
+    async = require('async'),
     bodyParser = require('body-parser'),
     SensorTag = require('sensortag');
 
 var app = express();
 
+
 function onDiscover(sensorTag) {
-    console.log('Discovered SensorTag:', sensorTag);
+    console.log('discovered: ' + sensorTag);
+
+    async.series([
+        function (callback) {
+            console.log('connectAndSetUp');
+            sensorTag.connectAndSetUp(callback);
+        },
+        function (callback) {
+            console.log('enableLuxometer');
+            sensorTag.enableLuxometer(callback);
+        },
+        function (callback) {
+            setTimeout(callback, 2000);
+        },
+        function (callback) {
+            sensorTag.on('luxometerChange', function (lux) {
+                console.log('\tlux = %d', lux.toFixed(1));
+            });
+
+            console.log('setLuxometer');
+            sensorTag.setLuxometerPeriod(2000, function (error) {
+                console.log('notifyLuxometer');
+                sensorTag.notifyLuxometer();
+            });
+            callback();
+        }
+    ]);
 }
+
+
+function handleError(err) {
+    console.log(err);
+}
+
+SensorTag.discoverAll(onDiscover);
 
 
 app.get('/', function (req, res) {
     res.send('Hello World!');
 });
 
-app.listen(3000, function () {
-    console.log('Sensorbot app listening on port 3000!')
+app.listen(2828, function () {
+    console.log('Sensorbot app listening on port 2828!')
 });
